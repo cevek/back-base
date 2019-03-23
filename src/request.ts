2723615dcba648ef8857d01a1b56cefa39e45995
cody <cevek@ya.ru>
2019-03-23 23:13:00 +0500

    Add attempts to request

--- src/request.ts
+++ src/request.ts
@@ -1,28 +1,56 @@
 import request = require('request');
+import { logger, JsonError } from './logger';
+import { sleep, never } from './utils';
 
-export function requestRaw(url: string, options?: request.CoreOptions) {
-	return new Promise<{ res: request.Response; body: Buffer }>((resolve, reject) =>
-		request(url, options, (err: Error | undefined, res, body: Buffer | undefined) => {
-			if (err) return reject(err);
-			if (res.statusCode < 200 || res.statusCode >= 400)
-				return reject(`Request "${url}", status code: ${res.statusCode}, body: ${body}`);
-			if (!body) return reject(`Request "${url}", response body is empty`);
-			resolve({ res, body });
-		}),
-	);
+export interface RequestOptions extends request.CoreOptions {
+	attemptsCount?: number;
+	attemptDelay?: number;
+}
+export async function requestRaw(url: string, options: RequestOptions = {}) {
+	logger.trace({ url, options }, 'Request start');
+	const { attemptsCount = 1, attemptDelay = 1000 } = options;
+	for (let i = 0; i < attemptsCount; i++) {
+		try {
+			
+			return await new Promise<{ res: request.Response; body: Buffer | string | object }>((resolve, reject) =>
+				request(url, options, (err: Error | string | undefined, res, body: Buffer | undefined) => {
+					logger.trace({ url, body }, 'Request response');
+					if (err) return reject(err);
+					if (!body || res.statusCode < 200 || res.statusCode >= 400)
+						return reject(new JsonError('Request reject', { url, statusCode: res.statusCode, body }));
+					resolve({ res, body });
+				}),
+			);
+		} catch (e) {
+			if (i < attemptsCount - 1) {
+				logger.trace(`Request error ${url}: ${e.message}`);
+				await sleep(attemptDelay);
+			} else {
+                Error.captureStackTrace(e);
+				throw e;
+			}
+		}
+	}
+	throw never();
 }
 
-export async function requestJSON<T>(url: string, options?: request.CoreOptions) {
+export async function requestJSON<T>(
+	url: string,
+	options?: RequestOptions,
+): Promise<{ data: T; res: request.Response }> {
 	const { res, body } = await requestRaw(url, {
 		headers: {
 			'content-type': 'application/json',
 		},
 		...options,
 	});
+	if (typeof body === 'object' && body !== null && !(body instanceof Buffer)) {
+		return { data: (body as unknown) as T, res };
+	}
 	try {
 		const json = JSON.parse(body.toString()) as T;
 		return { data: json, res };
-	} catch (e) {
-		throw new Error(`Response from "${url}" in not json: ${JSON.stringify(body.toString())}`);
+	} catch {
+		throw new JsonError(`Response in not json`, { url, body });
 	}
 }
