8935b873084cba71391c5c8c3b3375d404df9601
cody <cevek@ya.ru>
2019-04-29 09:56:43 +0000

    Add email notifications in logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,12 @@
 import mkdirp from 'mkdirp';
-import { createWriteStream, fstatSync, openSync, renameSync, readFileSync } from 'fs';
+import { createWriteStream, fstatSync, openSync, renameSync } from 'fs';
 import { dirname } from 'path';
 import words from './words';
 import { IncomingMessage, ClientRequest } from 'http';
 import colors from 'colors';
 import findUp from 'find-up';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import nodemailer from 'nodemailer';
 
 export class BaseException<T> extends Error {
 	kind: string;
@@ -19,99 +21,46 @@ export class Exception<T = object> extends BaseException<T> {}
 
 type Levels = keyof typeof levels;
 
-interface LoggerSettings {
-	streams: { level: Levels; file?: string; stdout?: boolean; rotate?: 'daily' }[];
+type LoggerStreamConfig =
+	| { level: Levels; type: 'file'; file: string; rotate?: 'daily' }
+	| { level: Levels; type: 'stdout' }
+	| {
+			level: Levels;
+			type: 'email';
+			options: SMTPTransport.Options;
+			from: string;
+			to: string;
+			subject: { start: string; error: string };
+	  };
+export interface LoggerSettings {
+	streams: LoggerStreamConfig[];
 }
 export class Logger {
-	protected files: {
-		level: number;
-		name: string;
-		stdout: boolean;
-		createdAt: Date;
-		stream: NodeJS.WritableStream;
-		dailyRotate: boolean;
-	}[] = [];
+	protected streams: LoggerStream[] = [];
 	constructor(protected settings: LoggerSettings) {
-		for (const stream of settings.streams) {
-			const level = levels[stream.level];
-			if (stream.file) {
-				mkdirp.sync(dirname(stream.file));
-				let createdAt = new Date();
-				try {
-					createdAt = fstatSync(openSync(stream.file, 'r')).ctime;
-				} catch (e) {}
-				const file = {
-					level,
-					name: stream.file,
-					stream: createWriteStream(stream.file),
-					createdAt,
-					stdout: false,
-					dailyRotate: stream.rotate === 'daily',
-				};
-				this.files.push(file);
+		if (settings.streams.length === 0) throw new Exception('Empty logger streams', { settings });
+		for (const streamConfig of settings.streams) {
+			const level = levels[streamConfig.level];
+			if (streamConfig.type === 'file') {
+				this.streams.push(new FileStream(level, streamConfig));
 			}
-			if (stream.stdout) {
-				this.files.push({
-					level,
-					stdout: true,
-					name: 'stdout',
-					stream: process.stdout,
-					createdAt: new Date(),
-					dailyRotate: false,
-				});
+			if (streamConfig.type === 'stdout') {
+				this.streams.push(new StdoutStream(level, streamConfig));
 			}
-		}
-		this.selectFile();
-	}
-
-	protected selectFile() {
-		const d = new Date();
-		for (const file of this.files) {
-			if (file.dailyRotate) {
-				const d2 = file.createdAt;
-				if (d.getDate() !== d2.getDate() || d.getMonth() !== d2.getMonth() || d.getFullYear() !== d2.getFullYear()) {
-					file.stream.end();
-					const historyName =
-						file.name.replace(/\.log$/, '') + '_' + file.createdAt.toISOString().split('T')[0] + '.log';
-					renameSync(file.name, historyName);
-					file.stream = createWriteStream(file.name);
-					file.createdAt = new Date();
-				}
+			if (streamConfig.type === 'email') {
+				this.streams.push(new EmailStream(level, streamConfig));
 			}
 		}
 	}
 
 	protected log(type: Levels, name: string, json?: object) {
-		this.selectFile();
 		if (!(json instanceof Object)) json = { raw: json };
 		const id = words[Math.floor(words.length * Math.random())];
 		const parentId = '';
-		const str = JSON.stringify([id, parentId, new Date(), type, name, json], jsonReplacer) + '\n';
-		for (const file of this.files) {
-			if (levels[type] <= file.level) {
-				if (file.stdout) {
-					let fn = colors.black;
-					if (type === 'error') fn = colors.red;
-					if (type === 'info') fn = colors.blue;
-					if (type === 'warn') fn = colors.yellow;
-					if (type === 'trace') fn = colors.gray;
-					if (type === 'external') fn = colors.magenta;
-					if (type === 'clientError') fn = colors.green;
-					const dt = new Date();
-					const dtS =
-						('0' + dt.getHours()).substr(-2) +
-						':' +
-						('0' + dt.getMinutes()).substr(-2) +
-						':' +
-						('0' + dt.getSeconds()).substr(-2);
-					file.stream.write(
-						colors.gray(dtS + ' ' + type + ' ') +
-							fn(name + ' ') +
-							colors.gray(JSON.stringify(json, jsonReplacer, 2) + '\n'),
-					);
-				} else {
-					file.stream.write(str);
-				}
+		const date = new Date();
+		for (const stream of this.streams) {
+			if (levels[type] <= stream.level) {
+				stream.write(id, parentId, date, type, name, json);
 			}
 		}
 	}
@@ -154,6 +103,110 @@ export class Logger {
 	}
 }
 
+abstract class LoggerStream {
+	constructor(public level: number) {}
+	abstract write(id: string, parentId: string, date: Date, type: Levels, name: string, json: object): void;
+}
+class EmailStream extends LoggerStream {
+	constructor(
+		level: number,
+		public options: {
+			options: SMTPTransport.Options;
+			from: string;
+			to: string;
+			subject: { start: string; error: string };
+		},
+	) {
+		super(level);
+		this.sendMail(this.options.subject.start, '');
+	}
+	transport = nodemailer.createTransport(this.options.options);
+	lastSendedAt = new Date(0);
+
+	sendMail(subject: string, text: string) {
+		this.transport
+			.sendMail({
+				to: this.options.to,
+				from: this.options.from,
+				subject,
+				text,
+			})
+			.catch(err => logger.error(err));
+	}
+
+	write(_id: string, _parentId: string, date: Date, type: Levels, name: string, json: object): void {
+		if (Date.now() - this.lastSendedAt.getTime() < 3_600_000) return;
+		this.sendMail(
+			this.options.subject.error,
+			`${date.toISOString()} ${type} ${name} ${JSON.stringify(json, jsonReplacer, 2)}`,
+		);
+		this.lastSendedAt = new Date();
+	}
+}
+class FileStream extends LoggerStream {
+	createdAt: Date;
+	stream: NodeJS.WritableStream;
+	rotate: 'daily' | 'never';
+	fileName: string;
+
+	constructor(level: number, public options: { file: string; rotate?: 'daily' }) {
+		super(level);
+		mkdirp.sync(dirname(options.file));
+		let createdAt = new Date();
+		try {
+			createdAt = fstatSync(openSync(options.file, 'r')).ctime;
+		} catch (e) {}
+		this.stream = createWriteStream(options.file);
+		this.createdAt = createdAt;
+		this.rotate = options.rotate || 'never';
+		this.fileName = options.file;
+	}
+
+	protected selectFile() {
+		const d = new Date();
+		if (this.rotate === 'daily') {
+			const d2 = this.createdAt;
+			if (d.getDate() !== d2.getDate() || d.getMonth() !== d2.getMonth() || d.getFullYear() !== d2.getFullYear()) {
+				this.stream.end();
+				const historyName =
+					this.fileName.replace(/\.log$/, '') + '_' + this.createdAt.toISOString().split('T')[0] + '.log';
+				renameSync(this.fileName, historyName);
+				this.stream = createWriteStream(this.fileName);
+				this.createdAt = new Date();
+			}
+		}
+	}
+
+	write(id: string, parentId: string, date: Date, type: Levels, name: string, json: object) {
+		this.selectFile();
+		const str = JSON.stringify([id, parentId, date, type, name, json], jsonReplacer) + '\n';
+		this.stream.write(str);
+	}
+}
+class StdoutStream extends LoggerStream {
+	constructor(level: number, _options: {}) {
+		super(level);
+	}
+	write(_id: string, _parentId: string, date: Date, type: Levels, name: string, json: object) {
+		let fn = colors.black;
+		if (type === 'error') fn = colors.red.bold;
+		if (type === 'info') fn = colors.cyan;
+		if (type === 'warn') fn = colors.yellow;
+		if (type === 'trace') fn = colors.gray;
+		if (type === 'external') fn = colors.magenta;
+		if (type === 'clientError') fn = colors.green;
+		const dtS =
+			('0' + date.getHours()).substr(-2) +
+			':' +
+			('0' + date.getMinutes()).substr(-2) +
+			':' +
+			('0' + date.getSeconds()).substr(-2);
+		process.stdout.write(
+			colors.gray(dtS + ' ' + type + ' ') + fn(name + ' ') + colors.gray(JSON.stringify(json, jsonReplacer, 2) + '\n'),
+		);
+	}
+}
+
 function jsonReplacer(_key: string, value: unknown) {
 	if (value instanceof Error) {
 		const stack = cleanStackTrace(value.stack);
@@ -189,25 +242,9 @@ const levels = {
 const packageJsonFile = findUp.sync('package.json', { cwd: require.main!.filename });
 if (!packageJsonFile) throw new Exception('package.json is not found');
 const projectDir = dirname(packageJsonFile);
+export const logger = new Logger(require(projectDir + '/logger.ts').default);
 
-const env = process.env.NODE_ENV || 'production';
-let settings: LoggerSettings | undefined;
-for (const e of ['', '.local', env, env + '.local']) {
-	try {
-		settings = JSON.parse(readFileSync(projectDir + '/logger' + e + '.json', 'utf8'));
-	} catch (e) {}
-}
-if (!settings) throw new Exception('Logger settings file is not found', { projectDir });
-for (const stream of settings.streams) {
-	if (!stream.file && !stream.stdout) {
-		throw new Exception('Logger settings.streams: file or stdout should be specified', { stream });
-	}
-	if (stream.rotate && stream.rotate !== 'daily') {
-		throw new Exception('Logger settings.streams: only daily rotate supports', { stream });
-	}
-}
-
-export const logger = new Logger(settings);
+// export const logger = new Logger(settings);
 
 const extractPathRegex = /\s+at.*?\((.*?)\)/;
 const pathRegex = /^internal|(.*?\/node_modules\/(ts-node)\/)/;
